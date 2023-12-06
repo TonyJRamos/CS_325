82b51bd8c89ddc0d2ba9106b212b43522a4ffa52
samh120 <115828589+samh120@users.noreply.github.com>
2023-12-05 18:39:06 -0800

    Update 325show-contents.sql

--- Team_Rocket/Samuel/325show-contents.sql
+++ Team_Rocket/Samuel/325show-contents.sql
@@ -3,18 +3,28 @@
 -- CS 325 - Fall 2023
 -- 12/4/2023
 
-spool 325result-contents.txt
+tee 325result-contents.txt
 
+SELECT 'User' AS Message;
 SELECT * FROM User;
+SELECT 'Customer' AS Message;
 SELECT *  FROM Customer;
+SELECT 'Address' AS Message;
 SELECT *  FROM Address;
+SELECT 'Catalog' AS Message;
 SELECT *  FROM Catalog;
+SELECT 'Orders' AS Message;
 SELECT *  FROM Orders;
+SELECT 'OrderDetails' AS Message;
 SELECT *  FROM OrderDetails;
+SELECT 'LineItems' AS Message;
 SELECT *  FROM LineItems;
-SELECT *  FROM Shipping;
+SELECT 'Shipment' AS Message;
+SELECT *  FROM Shipment;
+SELECT 'SubscriptionTemplate' AS Message;
 SELECT *  FROM SubscriptionTemplate;
+SELECT 'SubscriptionOrders' AS Message;
 SELECT *  FROM SubscriptionOrders;
 
-spool off
+notee
 
